cdb4646c6f9d24877835247bbfba5178804cd70e
M Morella <70173714+morellam-dev@users.noreply.github.com>
2020-08-24 17:57:47 -0400

    Add caesar cipher solution

---
+++ 02_caesar_cipher.js
@@ -0,0 +1,4 @@
+// 111 bytes + input length
+// Prints all 25 ROT N shifts of the input string, S
+
+S="Tqxxa iadxp!";for(i=26;--i;)console.log(S=S.replace(/[a-z]/gi,c=>String.fromCharCode(c.charCodeAt()-1+26*/a/i.test(c))))

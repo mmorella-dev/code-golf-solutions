c9a63e4ff781c80e8b3b450b23a790b944e0b13b
M Morella <70173714+morellam-dev@users.noreply.github.com>
2020-08-24 18:07:49 -0400

    add caveat to first script

--- 01_password_generator.js
+++ 01_password_generator.js
@@ -1,3 +1,4 @@
 // 61 bytes — Generates a random string of 9 ASCII characters
+// only outputs when copy-pasted into interactive the Node repl.
 
 for(i=s='';9>i++;)s+=String.fromCharCode(95*Math.random()+32)
